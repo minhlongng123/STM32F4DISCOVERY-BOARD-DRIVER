e86180655158ace710f55d79a429ed445201b0fe
Minh Long Nguyen <70574395+minhlongng123@users.noreply.github.com>
2021-09-10 23:53:03 +1000

    Create stm32f4xx_rcc_driver.h

---
+++ header/stm32f4xx_rcc_driver.h
@@ -0,0 +1,16 @@
+/*
+ * stm32f4xx_rcc_driver.h
+ *
+ *  Created on: Jul 12, 2021
+ *      Author: Minh Long Nguyen
+ */
+
+#ifndef INC_STM32F4XX_RCC_DRIVER_H_
+#define INC_STM32F4XX_RCC_DRIVER_H_
+
+#include "stm32f4xx.h"
+
+uint32_t RCC_GetPclk1value();
+uint32_t RCC_GetPclk2value();
+
+#endif /* INC_STM32F4XX_RCC_DRIVER_H_ */
